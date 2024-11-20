6443ffd8b562556f57e7c9129dc7519588088025
farazkhan2000 <f.skipper14@gmail.com>
2024-11-20 21:14:44 +0500

    icons changes

--- src/assets/icons/assets.js
+++ src/assets/icons/assets.js
@@ -1,10 +1,10 @@
-import dexscreener from './dexscreener.webp';
+import dexscreener from './dexscreener.png';
 import dextool from './dextool.webp';
 import gate from './gate.webp';
 import jupiter from './jupiter.webp';
 import moonshot from './moonshot.webp';
-import telegram from './telegram.webp';
-import twitter from './twitter.webp';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
 
 export const icons = {
   dexscreener,
